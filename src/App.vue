a8120d5bc9163a66aba9538f9212f43f559e75ba
chullaff <regina.deresh@gmail.com>
2023-06-05 04:50:53 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,128 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col col="7">
+            <ToDoList
+              v-bind:todoList="todoList"
+              @setUpdateTodo="setUpdateTodo"
+              @deleteTodo="deleteTodoBI"
+            />
+          </v-col>
+          <v-col col="3">
+            <v-text-field
+              width="400px"
+              label="Title"
+              v-model="inputTitle"
+              outlined
+              class="shrink"
+            >
+            </v-text-field>
+            <v-text-field
+              width="400px"
+              label="Description"
+              v-model="inputDescription"
+              outlined
+              class="shrink"
+            >
+            </v-text-field>
+            <v-btn
+              v-if="!isUpdate"
+              @click="createTodo()"
+              style="margin-right: 10px"
+              small
+              color="green"
+              :disabled="inputTitle.length && inputDescription.length <= 0"
+              >Создать
+            </v-btn>
+            <v-btn
+              v-if="isUpdate"
+              @click="updateTodo()"
+              small
+              color="yellow"
+              :disabled="inputTitle.length && inputDescription.length <= 0"
+              >Обновить
+            </v-btn>
+            <v-btn @click="deleteTodos()" style="align: center" color="red"
+              >Удалить всё
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDoList from "./components/ToDoList.vue";
+const axios = require("axios").default;
+const url = "http://localhost:3100/todos";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    ToDoList,
+  },
+
+  data: () => ({
+    isUpdate: false,
+    inputTitle: "",
+    inputDescription: "",
+    updateId: "",
+    todoList: [],
+  }),
+  methods: {
+    reset() {
+      this.inputTitle = "";
+      this.inputDescription = "";
+      this.isUpdate = false;
+      this.updateId = "";
+    },
+
+    async getTodos() {
+      const newTodo = await axios.get(url);
+      this.todoList = newTodo.data.todoList;
+    },
+
+    async deleteTodos() {
+      await axios.delete(url);
+      this.reset();
+      this.getTodos();
+    },
+
+    async deleteTodoBI(id) {
+      await axios.delete(url + "/" + id);
+      this.reset();
+      this.getTodos();
+    },
+
+    async createTodo() {
+      await axios.post(url, {
+        title: this.inputTitle,
+        description: this.inputDescription,
+      });
+      this.reset();
+      this.getTodos();
+    },
+
+    async setUpdateTodo(id) {
+      this.isUpdate = true;
+      const todo = await axios.get(url + "/" + id);
+      this.inputTitle = todo.data.todo.title;
+      this.inputDescription = todo.data.todo.description;
+      this.updateId = id;
+    },
+
+    async updateTodo() {
+      await axios.patch(url + "/" + this.updateId, {
+        title: this.inputTitle,
+        description: this.inputDescription,
+      });
+      this.reset();
+      this.getTodos();
+    },
+  },
+};
+</script>
---
+++ src/components/ToDoList.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-card v-for="todo in todoList" :key="todo">
+    <v-card-title>{{ todo.title }}</v-card-title>
+    <v-card-text>{{ todo.description }}</v-card-text>
+    <v-card-actions class="justify-end">
+      <v-btn
+        variant="text"
+        color="teal-accent-4"
+        @click="$emit('setUpdateTodo', todo.id)"
+        icon="mdi-square-edit-outline"
+      ></v-btn>
+      <v-btn
+        variant="text"
+        color="teal-accent-4"
+        @click="$emit('deleteTodo', todo.id)"
+        icon="mdi-trash-can-outline"
+      ></v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+<script>
+export default {
+  props: ["todoList"],
+  methods: {},
+};
+</script>
+
+<style scoped>
+.v-card {
+  margin-bottom: 10px;
+}
+</style>
